5b3b13f63c03c4035f2fabffa0b123185ab78bd2
reynaldosabbatini <reinaldodiprovet@gmail.com>
2024-08-05 22:51:01 -0300

    android
    andr

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, imagen } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    
+      <Text style={{ color: '#fff' }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#25292e',
+      alignItems: 'center',
+    },
+    imageContainer: {
+      flex: 1,
+      paddingTop: 58,
+    },
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
 });
