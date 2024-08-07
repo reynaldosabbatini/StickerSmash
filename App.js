978b4eaf5d77c538cb5d0314686f501caeae3306
reynaldosabbatini <reinaldodiprovet@gmail.com>
2024-08-07 20:40:31 -0300

    button

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, imagen } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -7,37 +11,35 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
-    
-      <Text style={{ color: '#fff' }}>
-        Open up App.js to start working on your app!
-      </Text>
+      <View style={styles.footerContainer}>
+      <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
-  }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#25292e',
-      alignItems: 'center',
-    },
-    imageContainer: {
-      flex: 1,
-      paddingTop: 58,
-    },
-    image: {
-      width: 320,
-      height: 440,
-      borderRadius: 18,
-    },
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
